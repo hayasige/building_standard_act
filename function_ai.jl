60a7172e526bf8eae499db22ac0e13e8747c69c8
hayashige <4336307+hayasige@users.noreply.github.com>
2021-01-23 14:58:37 +0900

    Update function_ai.jl

--- function_ai.jl
+++ function_ai.jl
@@ -5,9 +5,9 @@ function ai(αi, T)
   if αi < 0 || 1 < αi
     throw(DomainError(αi, "αi：範囲外."))
   end
-  if T < 0.
+  if T < 0
     throw(DomainError(T, "T：範囲外."))
   end
-  1. +(1. / sqrt(αi) - αi ) * 2T /(1. + 3T)
+  1 +(1 / sqrt(αi) - αi ) * 2T /(1 + 3T)
 end
 # eof
