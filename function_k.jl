2de0bf07126423e559ddb722cfaa7ab8e8ac4286
hayashige <4336307+hayasige@users.noreply.github.com>
2021-01-24 20:17:47 +0900

    Update function_k.jl

--- function_k.jl
+++ function_k.jl
@@ -1,20 +1,16 @@
+function k(H, Z) 
+
 # function_k.jl
 # 8:22 2021/01/02
+# 令88条4項
+# k 水平震度
+# H 建築物の地下部分の各部分の地盤面からの深さ(m)
+# Z 令88条1項に規定するZの数値
 
-function k(H, Z) # 令88条4項
-  # k 水平震度
-  # H 建築物の地下部分の各部分の地盤面からの深さ(m)
-  # Z 令88条1項に規定するZの数値
-  if H < 0.
-    throw(DomainError(H, "H: 範囲外."))
+  if H > 20
+    H = 20
   end
-  if Z < 0. || 1. < Z
-    throw(DomainError(Z, "Z: 範囲外."))
-  end
-  if H > 20.
-    H = 20.
-  end
-  0.1 * (1. - H / 40.) * Z
+  0.1 * (1 - H / 40) * Z
 end
 # eof
   
