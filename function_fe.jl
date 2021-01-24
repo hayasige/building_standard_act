1131a51390c45bfe7d511102e21cdffb55a03084
hayashige <4336307+hayasige@users.noreply.github.com>
2021-01-24 20:17:30 +0900

    Update function_fe.jl

--- function_fe.jl
+++ function_fe.jl
@@ -1,20 +1,18 @@
-# function Fe(Re)
-# 昭和55年建設省告示第1972号第7表2
-# Fe 
+function Fe(Re)
+
+# 昭和55年建設省告示第1972号第7表2 
 # Re 偏心率
 
   if Re <= 1
-    1
+    return 1
   elseif 0.15 < Re < 0.3
-    x1 = 0.15
-    y1 = 1
-    x2 = 0.3
-    y2 = 1.5
+    x1, y1 = 0.15, 1
+    x2, y2 = 0.3,  1.5
     N1 = (x2 - Re) / (x2 - x1)
     N2 = (Re - x1) / (x2 - x1)
-    N1 * y1 + N2 * y2
-  else
-    1.5
+    return N1 * y1 + N2 * y2
+  elseif 0.3 <= Re
+    return 1.5
   end
 end
 # eof
