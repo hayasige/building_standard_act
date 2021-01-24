6a37165049b5b30abdf4172200948d4a14fea051
hayashige <4336307+hayasige@users.noreply.github.com>
2021-01-24 20:17:35 +0900

    Update function_fs.jl

--- function_fs.jl
+++ function_fs.jl
@@ -1,12 +1,10 @@
-# function Fs(Rs)
+function Fs(Rs)
+
 # 昭和55年建設省告示第1972号第7表1
 # Fs 
 # Rs 剛性率
 
-  if Rs => 0.6
-    1
-  elseif Rs < 0.6
-    2 - Rs / 0.6
-  end
+  Rs => 0.6  && return 1
+  Rs  < 0.6  && return 2 - Rs / 0.6
 end
 # eof
