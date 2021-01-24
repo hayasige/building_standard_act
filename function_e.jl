e1c747ae2e95a29f0ff4718ecaae6adf75447842
hayashige <4336307+hayasige@users.noreply.github.com>
2021-01-24 20:17:16 +0900

    Update function_e.jl

--- function_e.jl
+++ function_e.jl
@@ -1,17 +1,13 @@
+function E(Er, Gf)
+
 # function_e.jl
 # 8:22 2021/01/02
+# H12建告1454第1
+# E 令87条2項に規定するE
+# Er 平均風速の高さ方向の分布を表す係数
+# Gf ガスト影響係数
 
-function E(Er, Gf) # H12建告1454第1
-  # 令87条2項に規定するE
-  # 平均風速の高さ方向の分布を表す係数
-  # ガスト影響係数
-  if Er <0
-    throw(DomainError(Er, "Er: 範囲外."))
-  end
-  if Gf <0
-    throw(DomainError(Gf, "Gf: 範囲外."))
-  end
-  return Er^2 * Gf
+  Er^2 * Gf
 end
 # eof
   
