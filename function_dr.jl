7d76f8986d4ce529e7263ca6799ab67d0a694d84
hayashige <4336307+hayasige@users.noreply.github.com>
2021-01-24 20:17:11 +0900

    Update function_dr.jl

--- function_dr.jl
+++ function_dr.jl
@@ -1,9 +1,10 @@
+function dr(x, y)
+
 # function_dr.jl
 # 19:33 2021/01/05
-
-function dr(x, y)# H19年国交告594号第2三号ホ
-  # x 最上端から最下端までの水平投影長さ(m)
-  # y 屋根勾配(度)
+# H19年国交告594号第2三号ホ
+# x 最上端から最下端までの水平投影長さ(m)
+# y 屋根勾配(度)
   hx = 50 - 10
   hy = 15 -  2
   N1 = (50-x) / hx * (15 - y) / hy
@@ -18,8 +19,4 @@ function dr(x, y)# H19年国交告594号第2三号ホ
   a = [a1, a2, a3, a4]
   (N * a)[1, 1]
 end
-
-println("x\ty\tdr")
-for (x, y) in [(10, 2), (10, 15), (50, 2), (50, 15)]
-  println("$x\t$y\t$(dr(x,y))")
-end
+# eof
